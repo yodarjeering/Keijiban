965a068a65749e42f7068b749054ed386bacdf8d
RINTARO <rince@RINTAROnoiMac.local>
2020-05-19 18:02:56 +0900

    8kaime

--- resources/views/keijiban/messages.blade.php
+++ resources/views/keijiban/messages.blade.php
@@ -6,4 +6,19 @@
 @section('content')
     <table>
         <tr><th>$thread_name</th></tr>
+        @foreach($thread_items as $thread_item)
+            <tr>
+                <td>{{$thread_item->getData()}}</td>
+            </tr>
+        @endforeach
+    </table>
+    <form action="keijiban/messages" method="post">
+        <input type="text" name "msg">
+        <input type="submit" value="投稿する">
+    </form>
+    <tr><th>form</th></tr>
+@endsection
 
+@section('footer')
+    copyright 2020 groupb.
+@endsection
