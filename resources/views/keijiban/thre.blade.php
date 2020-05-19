c3ca15a3dc3309dc12021200a13d84d6fcbe5194
tom-takeru <tom.2461.ty@gmail.com>
2020-05-20 00:18:06 +0900

    modify thre

--- resources/views/keijiban/thre.blade.php
+++ resources/views/keijiban/thre.blade.php
@@ -5,20 +5,24 @@
 
 @section('content')
     @if(isset($items))
-        @foreach($items as $item)
-            <li>
-                <div>{{$item->date}}</div>
-                <a href="/keijiban/thre?thread_id={{$item->id}}">{{$item->name}}</a>
-            </li>
-        @endforeach
+        <ul>
+            @foreach($items as $item)
+                <li>
+                    <div>{{$item->date}}</div>
+                    <a href="/keijiban/thre?thread_id={{$item->id}}">{{$item->name}}</a>
+                </li>
+            @endforeach
+        </ul>
     @endif
 
     <form action="keijiban/messages" method="post">
         @csrf
-        <input type="text" name = "msg">
+        <input type="text" name="msg">
         <input type="submit" value="投稿する">
     </form>
-    <tr><th>form</th></tr>
+    <tr>
+        <th>form</th>
+    </tr>
 @endsection
 
 @section('footer')
