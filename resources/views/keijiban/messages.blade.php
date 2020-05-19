e11c36dae200954c9f9f5597b3dd4b4163cbb45a
RINTARO <rince@RINTAROnoiMac.local>
2020-05-19 17:31:39 +0900

    3kaime

--- resources/views/keijiban/messages.blade.php
+++ resources/views/keijiban/messages.blade.php
@@ -1,4 +1,24 @@
-<?php
-    //んごご
-    asss
-    ajfjfhfh
+@extends('layouts.keijibanapp')
+
+@section('title','１２ちゃんねる')
+
+
+@section('content')
+    <table>
+        <tr><th>$thread_name</th></tr>
+        @foreach($thread_items as $thread_item)
+            <tr>
+                <td>{{$thread_item->getData()}}</td>
+            </tr>
+            @endforeach
+    </table>
+    <form action="keijiban/messages" method="post">
+    <input type="text" name "msg">
+    <input type="submit" value="投稿する">
+    </form>
+    <tr><th>form</th></tr>
+    @endsection
+
+@section('footer')
+    copyright 2020 groupb.
+    @endsection
