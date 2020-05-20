f1337c9a6c68937c747faebd8de0b21571423625
tom-takeru <tom.2461.ty@gmail.com>
2020-05-20 09:38:00 +0900

    modify page 1

--- app/Message.php
+++ app/Message.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
+    public $timestamps = false;
     public function getData()
     {
         return $this->content  .' '. $this->time ;
