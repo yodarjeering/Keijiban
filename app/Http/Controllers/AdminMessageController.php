55514bad7c6074a26345d52bc07b0152f3807dae
yusuke1390 <yughs0624.1390@gmail.com>
2020-05-20 14:02:27 +0900

    admin thre

--- app/Http/Controllers/AdminMessageController.php
+++ app/Http/Controllers/AdminMessageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Thread;
+use App\Message;
 use Illuminate\Http\Request;
 
 class AdminMessageController extends Controller
--- resources/views/keijiban/admin_thre.blade.php
+++ resources/views/keijiban/admin_thre.blade.php
@@ -5,26 +5,25 @@
 @section('content')
     <form action="/keijiban/admin_thre" method="post">
         @csrf
-        <input type="text" name="input" value ="{{$input}}">
+        <input type="text" name="input" value ="{{$input ?? ''}}">
         <input type="submit" value="find">
     </form>
 
     <h2>{{$thread->name}}</h2>
     @if(isset($items))
+        @php
+            $number = 1;
+        @endphp
         <table>
             <tr><th>data</th></tr>
-            @php
-                $number = 1;
-            @endphp
             @foreach($items as $item)
                 <tr>
-                    <a href="/keijiban/admin_message_delete?message_id={{$item->id}}">
-                        <td>$number++ . ': '{{$item->getData()}}</td></a>
+                        <td><a href="/keijiban/admin_message_delete?id={{$item->id}}">{{$number++}}: {{$item->getData()}}</a></td>
                 </tr>
             @endforeach
         </table>
     @endif
-    <a href="/keijiban">戻る</a>
+    <a href="/keijiban/admin_index">戻る</a>
 @endsection
 
 @section('footer')
