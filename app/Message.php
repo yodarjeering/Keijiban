66c9f41fe5eda0462ca0c1f2acb9faa02cf23364
isemoto <isemoca.555@gmail.com>
2020-05-20 09:39:16 +0900

    iroirp

--- app/Message.php
+++ app/Message.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
+    protected  $guarded = array('id');
     public function getData()
     {
         return $this->content  .' '. $this->time ;
--- app/Thread.php
+++ app/Thread.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
+    protected  $guarded = array('id');
     public function getData()
     {
         return $this->name . '\n' . $this->time;
